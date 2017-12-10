ab27f335f8a52f5f0573a6f914a6099f8f095443
Aurélien Roux <aureroux13@me.com>
2017-12-10 12:36:45 -0500

    create a second var template in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,10 +3,54 @@
 console.log('app is running');
 
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'my App'
+  React.createElement(
+    'h1',
+    null,
+    'my App'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'this is app'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'One'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Two'
+    )
+  )
 );
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Aur\xE9lien Roux'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: 36'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: montreal'
+  )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 console.log('app is running');
 
-var template = <h1>my App</h1>;
+var template = (
+  <div>
+  <h1>my App</h1>
+  <p>this is app</p>
+  <ol>
+  <li>One</li>
+  <li>Two</li>
+  </ol>
+  </div>
+);
+
+var templateTwo = (
+  <div>
+  <h1>Aurélien Roux</h1>
+  <p>Age: 36</p>
+  <p>Location: montreal</p>    
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
