c7d44af1274ef3d706778010d9188936183c8672
Aurélien Roux <aureroux13@gmail.com>
2019-01-29 20:46:33 -0500

    es6 subclasses

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,26 +1,62 @@
-
 class Person {
   constructor(name = 'anonymous', age = 0) {
     this.name = name;
     this.age = age;
   }
-
   getGretting() {
-     return `Hi ${this.name}!`;
+    return `Hi I am ${this.name}!`;
   }
-
   getDescription() {
-    return `${this.name} is ${this.age} years old`;
+    return `${this.name} is ${this.age} years old.`;
   }
 }
 
-const me = new Person('Aurelien Roux', 37);
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age)
+    this.major = major
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription()
+    if (this.hasMajor()) {
+      description += ` He study in ${this.major}`
+    }
+    return description;
+  }
+}
+
+class Traveler extends Person {
+  constructor(name, age, homeLocation){
+    super(name, age)
+    this.homeLocation = homeLocation
+  }
+  hasHomeTown() {
+    return !!this.homeLocation
+  }
+  getGreeting() {
+    let description = super.getGretting()
+    if (this.hasHomeTown()) {
+      description += ` I am from ${this.homeLocation}`
+    }
+    return description
+  }
+}
+
+const me = new Student('Aurelien Roux', 37, 'javascript');
 console.log(me);
-
-const other = new Person()
-console.log(other);
-
-console.log(me.getGretting());
 console.log(me.getDescription());
-console.log(other.getGretting());
+
+const other = new Student()
+console.log(other);
 console.log(other.getDescription());
+
+const traveler = new Traveler('David', 49, 'France')
+console.log(traveler);
+console.log(traveler.getGreeting());
+
+const travelerTwo = new Traveler('Adrien', 6)
+console.log(travelerTwo);
+console.log(travelerTwo.getGreeting());
