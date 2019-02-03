e555a38cf50bdd90eadb4f19f47b69ce7c93ba61
Aurélien Roux <aureroux13@gmail.com>
2019-02-03 16:40:19 -0500

    addOption component with state

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ class IndecisionApp extends React.Component {
     super(props)
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: ['one', 'two', 'three']
+      options: []
     };
   }
 
@@ -22,6 +23,20 @@ class IndecisionApp extends React.Component {
     alert(option)
   }
 
+  handleAddOption(option) {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'already exists';
+    }
+
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat(option)
+      }
+    })
+  }
+
   render() {
     const title = 'Indecision';
     const subtitle = 'choice app';
@@ -37,7 +52,9 @@ class IndecisionApp extends React.Component {
           options={this.state.options} 
           handleDeleteOptions={this.handleDeleteOptions}
         />
-        <AddOption />
+        <AddOption 
+          handleAddOption={this.handleAddOption}
+        />
       </div>
     )
   }
@@ -93,16 +110,28 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    };
+  }
+
   handleAddOption(e) {
     e.preventDefault()
     const option = e.target.elements.option.value.trim()
-    if (option) {
-      alert(option)
-    }
+    const error = this.props.handleAddOption(option);
+
+    this.setState(() => {
+      return { error }
+    })
   }
+
   render() {
     return (
       <div>
+        {this.state.error && <p>{this.state.error}</p> }
         <form onSubmit={this.handleAddOption}>
           <input type='text' name='option'/>
           <button type='submit'>Add option</button>
