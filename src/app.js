2eaea0e88b00671bd713162df7237ebc1703b8bc
Aurélien Roux <aroux@clever-age.com>
2019-02-04 13:11:03 -0500

    stateless functional component

--- src/app.js
+++ src/app.js
@@ -60,53 +60,45 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    )
-  } 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  )
 }
 
-class Action extends React.Component {
-  render() {
-    return (
-      <div>
-        <button 
-          onClick={this.props.handlePick}
-          disabled={!this.props.hasOptions}
-        >
-          What to do ?
-        </button>
-      </div>
-    )
-  }
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What to do ?
+      </button>
+    </div>
+  )
 }
 
-class Options extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>
-          Remove all
-        </button>
-        {this.props.options.map((option, index) => <Option key={index} text={option} /> )}
-      </div>
-    )
-  }
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>
+        Remove all
+      </button>
+      {props.options.map((option, index) => <Option key={index} text={option} /> )}
+    </div>
+  )
 }
 
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-        Option: {this.props.text}
-      </div>
-    )
-  }
+const Option = (props) => {
+  return (
+    <div>
+      Option: {props.text}
+    </div>
+  )
 }
 
 class AddOption extends React.Component {
