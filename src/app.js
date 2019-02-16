e7b7ca60f6c4ab90aebc7084547ceb9f684c6702
Aurélien Roux <aureroux13@gmail.com>
2019-02-15 20:15:41 -0500

    import and export files es6

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('is running again');
+import { square, add } from './utils';
+import { isAdult, canDrink } from './person';
+
+console.log(square(3));
+console.log(add(9, 5));
+
+console.log(isAdult(19));
+console.log(canDrink(19));
+
---
+++ src/utils.js
@@ -0,0 +1,4 @@
+export const square = (x) => x * x;
+export const add = (a, b) => a + b;
+
+// export { square, add };
