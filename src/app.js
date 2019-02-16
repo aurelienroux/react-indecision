1b57664426004ed38c5503e585d9be0ba93c8176
Aurélien Roux <aureroux13@gmail.com>
2019-02-15 21:07:38 -0500

    import npm module

--- src/app.js
+++ src/app.js
@@ -1,11 +1,6 @@
-// import subtract, { square, add } from './utils';
-// console.log(square(3));
-// console.log(add(9, 5));
-// console.log(subtract(50, 25));
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+const template = React.createElement('p', {}, 'testing123');
 
-import isSenior, { isAdult, canDrink } from './person';
-console.log('is adult', isAdult(19));
-console.log('can drink', canDrink(19));
-console.log('is senior', isSenior(66));
-
+ReactDOM.render(template, document.getElementById('app'));
