a1bf2f46101823a62e84261bc350715dd33f7844
Aurélien Roux <aureroux13@gmail.com>
2019-06-27 21:52:16 -0400

    style add options widget

--- src/components/Options.js
+++ src/components/Options.js
@@ -3,22 +3,25 @@ import Option from './Option';
 
 const Options = (props) => (
   <div>
-    <button 
-      className="button button--link"
-      onClick={props.handleDeleteOptions}
-    >
-      Remove all
-    </button>
-    {props.options.length === 0 && <p>Please add an option</p> }
-    <ul>
-      {props.options.map((option, index) => (
+    <div className="widget-header">
+      <h3 className="widget-header__title">Your options</h3>
+      <button 
+        className="button button--link"
+        onClick={props.handleDeleteOptions}
+      >
+        Remove all
+      </button>
+    </div>
+    {props.options.length === 0 && <p className="widget__message">Please add an option</p> }
+    {
+      props.options.map((option, index) => (
         <Option 
           key={index} 
           text={option} 
           handleDeleteOption={props.handleDeleteOption} 
         />
-      ))}
-    </ul>
+      ))
+    }
   </div>
 )
 
